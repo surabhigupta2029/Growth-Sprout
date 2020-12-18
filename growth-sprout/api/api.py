15fb6b03c7a8080d4e7ae6915f0a16477a0ca645
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-12-18 11:46:31 -0800

    edited flask

--- growth-sprout/api/api.py
+++ growth-sprout/api/api.py
@@ -1,8 +1,13 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+
+@app.route('/weather', methods=["GET"])
+def get_weather():
+    req = request.get_data("http://api.openweathermap.org/data/2.5/weather?q=campbell,ca,us&appid=29a2d65b7d930665b8c87efbbfd86372")
+    print(req)
+    return "yay"
+
+app.run()
