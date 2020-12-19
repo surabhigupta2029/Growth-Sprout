3f4f4eaea746e90ce60a98a5cf88c4b21cb2865a
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-12-18 20:24:10 -0800

    finished frontend

--- growth-sprout/src/components/MyForm.js
+++ growth-sprout/src/components/MyForm.js
@@ -1,52 +1,91 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./MyForm.css";
 
 export default class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      USstate: '',
-      city: '',
-      zipcode: null,
+      USstate: "",
+      city: "",
+      zipcode: null
     };
   }
-  mySubmitHandler = (event) => {
+  mySubmitHandler = event => {
     event.preventDefault();
     let zipcode = this.state.zipcode;
     if (!Number(zipcode)) {
       alert("Your zipcode must be a number");
     }
-    alert("The information you provided state: " +  this.state.USstate + " city: " + this.state.city + " zipcode: " + this.state.zipcode)
-  }
-  myChangeHandler = (event) => {
+    alert(
+      "The information you provided state: " +
+        this.state.USstate +
+        " city: " +
+        this.state.city +
+        " zipcode: " +
+        this.state.zipcode
+    );
+  };
+  myChangeHandler = event => {
     let nam = event.target.name;
     let val = event.target.value;
-    this.setState({[nam]: val});
-  }
+    this.setState({ [nam]: val });
+  };
   render() {
     return (
-      <form onSubmit={this.mySubmitHandler}>
-      <p>Enter your state:</p>
-      <input
-        type='text'
-        name='USstate'
-        onChange={this.myChangeHandler}
-      />
-      <p>Enter your city:</p>
-      <input
-        type='text'
-        name='city'
-        onChange={this.myChangeHandler}
-      />
-      <p>Enter your zipcode:</p>
-      <input
-        type='text'
-        name='zipcode'
-        onChange={this.myChangeHandler}
-      />
-      <br/>
-      <br/>
-      <input type='submit' />
-      </form>
+      <div className="location-form">
+        <Container>
+          <form onSubmit={this.mySubmitHandler}>
+            <Row>
+              <Col>
+                <p className="form-label">STATE</p>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  name="USstate"
+                  placeholder="Enter state."
+                  className="input-bar"
+                  onChange={this.myChangeHandler}
+                />
+              </Col>
+            </Row>
+            <Row className="per-row">
+              <Col>
+                <p className="form-label">CITY</p>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="Enter city."
+                  className="input-bar"
+                  onChange={this.myChangeHandler}
+                />
+              </Col>
+            </Row>
+            <Row className="per-row">
+              <Col>
+                <p className="form-label">ZIPCODE</p>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  name="zipcode"
+                  placeholder="Enter zipcode."
+                  className="input-bar"
+                  onChange={this.myChangeHandler}
+                />
+              </Col>
+            </Row>
+            <Row className="per-row">
+              <Col>
+                <input className="sub-but" type="submit" />
+              </Col>
+            </Row>
+          </form>
+        </Container>
+      </div>
     );
   }
 }
